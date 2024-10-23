0292b5086284850f0d9d0cdff249fdb0de2a4444
YVictorin <vicyonjou@gmail.com>
2024-10-23 19:06:40 -0400

    made sponsorship page semi-responsive

--- js/sponsorship.js
+++ js/sponsorship.js
@@ -1,5 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
     const elems = document.querySelectorAll('.scrollspy');
     M.ScrollSpy.init(elems);
+
+
+    const allTheTabsElems = document.querySelector('.tabs')
+    const instance = M.Tabs.init(allTheTabsElems, {
+        swipeable : true,
+    });
     
 });
+
+
+
+
+
