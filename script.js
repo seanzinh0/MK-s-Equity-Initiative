49189c44f1ded45c28375b5bf4548255e7a04e3a
Tylerk2565 <ty25566@gmail.com>
2024-10-22 23:26:30 -0400

    set up modal + its jquery for the donations page

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 $(document).ready(function () {
 	$(".sidenav").sidenav();
 });
+
+$(document).ready(function(){
+	$('select').formSelect();
+});
+
+$(document).ready(function(){
+	$('.modal').modal();
+});
+
+// Modal submit on agree eventlistner 
+// Agree btn on click, function takes info from form, checks if valid, if true, it submits
+$('#agree-btn').on('click', function() {
+	var form = $('.donations-form');
+
+	if (form[0].checkValidity()) {
+		form.submit();
+	}
+});
